9edfc0ace978c526d3d2f13afeaaf1367915aef6
Martijn <oudmartijn@gmail.com>
2018-04-04 21:10:57 +0200

    Lightbox preview, new logos

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ a {
     50%{background-position:100% 50%}
     100%{background-position:0% 50%}
 }
-@keyframes Scroll { 
+@keyframes Scroll {
     0%{background-position:0% 50%}
     50%{background-position:100% 50%}
     100%{background-position:0% 50%}
@@ -69,27 +69,60 @@ a {
     color: #fff;
 }
 
-.candidate {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+@media only screen and (min-width: 1024px) {
+    .candidate {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 }
 
 .candidate--logo {
     display: flex;
     flex-direction: column;
-    justify-content: center;   
+    justify-content: center;
+    text-align: center;
+}
+
+.candidate--logo img {
+    padding-top: 5rem;
+    padding-bottom: 5rem;
 }
 
 .candidate--details {
-    box-shadow: 0 0 90px 0 rgba(3,34,71,.08);
+    box-shadow: 0 0 90px 0 rgba(0, 30, 70, .08);
     padding: 60px 60px;
+}
+
+.candidate--details-pos {
+    color: #ccc;
+    font-weight: lighter;
 }
 
 .candidate p {
     padding-top: 1rem;
     padding-bottom: 1rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+.fadeIn {
+  animation-name: fadeIn;
+}
+
+#lightbox-container {
+    animation: fadeIn 0.4s ease;
 }
 </style>
 </head>
@@ -98,65 +131,87 @@ a {
 <section class="section introduction">
 <div class="container is-fullhd has-text-centered">
     <h1 class="title">Vote on a new logo for Whitecoin.</h1>
-    <p>We are working hard on a new identity for Whitecoin. With that comes a new logo. Help us pick a new logo by voting for your favourite. Next to the each logo is an uniqe voting wallet. To vote you simply donate to the wallet to cast a vote. 1 XWC equals 1 vote. At the end of the voting the candidate with the most votes will be the new logo.</p>
+    <p>We are working hard on a new identity for Whitecoin. With that comes a new logo. Help us pick a new logo by voting for your favourite. Next to the each logo is an uniqe voting wallet. To vote you simply donate to the wallet to cast a vote. 1 XWC equals 1 vote. At the end of the voting the candidate with the most votes wins.</p>
 </div><!-- /container -->
 </section>
 
 <?php
 $arrCandidates = [];
 $arrCandidates[] = [
+    'pos' => 1,
     'title' => 'Candidate 1',
     'description' => 'Sleek and modern logo that resembles a \'C\' and a coin. ',
     'filename' => 'xwc-logo-candidate-1.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 2,
     'title' => 'Candidate 2',
     'description' => 'The two checkmarks that spell out a W give this logo a very positive and graph like effect.',
     'filename' => 'xwc-logo-candidate-2.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 3,
     'title' => 'Candidate 3',
     'description' => 'The double stripes give this logo a real currency vibe. An uncomplicated yet recognizable shape with a nod back to other payment methods.',
     'filename' => 'xwc-logo-candidate-3.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 4,
     'title' => 'Candidate 4',
     'description' => 'The W in this logo is a positive trend line / graph to resemble the growth XWC has had so far.',
     'filename' => 'xwc-logo-candidate-4.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 5,
     'title' => 'Candidate 5',
     'description' => 'Multiplying and interconnected nodes that spell out a W. This resembles the nodes and connections of a blockchain.',
     'filename' => 'xwc-logo-candidate-5.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 6,
     'title' => 'Candidate 6',
     'description' => 'A stylized W curled like DNA strands.',
     'filename' => 'xwc-logo-candidate-6.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
 $arrCandidates[] = [
+    'pos' => 7,
     'title' => 'Candidate 7',
     'description' => 'Variant on Candidate 3 but encircled so the shape further resembles a coin.',
     'filename' => 'xwc-logo-candidate-7.png',
     'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
 ];
+$arrCandidates[] = [
+    'pos' => 8,
+    'title' => 'Candidate 8',
+    'description' => 'Alternative to Candidate 7.',
+    'filename' => 'xwc-logo-candidate-8.png',
+    'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
+];
+$arrCandidates[] = [
+    'pos' => 9,
+    'title' => 'Candidate 9',
+    'description' => 'Variant on Candidate 5 but simplified.',
+    'filename' => 'xwc-logo-candidate-9.png',
+    'wallet' => 'QPVcNhtNHowe5zdbKMYMBM25RHyAcNUDV',
+];
 foreach ($arrCandidates as $candidate) {
+
 ?>
-    <section class="section candidate">
+    <section class="section candidate" id="candidate-<?=$candidate['pos'];?>">
     <div class="container is-fullhd">
         <div class="columns">
             <div class="column is-6 candidate--logo">
                 <a href="/logos/<?=$candidate['filename'];?>" title="Click to preview in full screen"><img src="/logos/<?=$candidate['filename'];?>" data-lightbox="/logos/<?=$candidate['filename'];?>" alt="XWC Logo Candidate: <?=$candidate['title'];?>"></a>
             </div><!-- /column -->
             <div class="column is-offset-2 is-4 candidate--details">
-                <h2 class="title"><?=$candidate['title'];?></h2>
-                <p><strong>Vote for this logo by donating to: </strong><a href="http://explorer2.whitecoin.info/address/<?=$candidate['wallet'];?>"><?=$candidate['wallet'];?></a></p>
+                <h2 class="title"><a class="is-hidden-mobile candidate--details-pos" href="/#candidate-<?=$candidate['pos'];?>">#</a> <?=$candidate['title'];?></h2>
+                <p><strong>Vote for this logo by donating to: </strong><a href="http://explorer2.whitecoin.info/address/<?=$candidate['wallet'];?>" title="<?=$candidate['wallet'];?>"><?=$candidate['wallet'];?></a></p>
 
                 <p><?=$candidate['description'];?></p>
             </div><!-- /column -->
@@ -180,6 +235,7 @@ function lightBox(imgsrc) {
         container.style.left = '0';
         container.style.top = '0';
         container.style.background = '#fff';
+        container.classList.add('fadeIn');
         document.body.appendChild(container);
 
         var image = document.createElement('img');
